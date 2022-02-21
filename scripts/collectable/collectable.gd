6b5462518f3adc9c34d8cf1d147510a45c3fc5d8
lorenzobartolini00 <s1092626@studenti.univpm.it>
2022-02-21 11:45:42 +0100

    Aggiunte le mesh delle armi e munizioni

--- scripts/collectable/collectable.gd
+++ scripts/collectable/collectable.gd
@@ -1,3 +1,4 @@
+tool
 extends Area
 
 class_name Collectable
@@ -5,14 +6,21 @@ class_name Collectable
 export(Resource) var item = item as Item
 export(int) var quantity
 
+onready var _set: bool = false
+
+func _process(delta):
+	if not _set:
+		_set = true
+		get_node("MeshInstance").mesh = item.mesh
 
 func _physics_process(delta):
-	var bodies = get_overlapping_bodies()
-	for body in bodies:
-		if body is Player:
-			GameEvents.emit_signal("collected", get_item(), get_quantity())
-			print(item.name + " collected")
-			queue_free()
+	if not Engine.editor_hint:
+		var bodies = get_overlapping_bodies()
+		for body in bodies:
+			if body is Player:
+				GameEvents.emit_signal("collected", get_item(), get_quantity())
+				print(item.name + " collected")
+				queue_free()
 
 
 func get_item() -> Item:
