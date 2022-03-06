ab9cd45b74f305fc4c23f429c5c0dec1bd8fea98
lorenzobartolini00 <s1092626@studenti.univpm.it>
2022-03-06 18:22:17 +0100

    Aggiunto TitleScreen

---
+++ scripts/title_screen/title_screen.gd
@@ -0,0 +1,12 @@
+extends Control
+
+class_name TitleScreen
+
+export(NodePath) onready var camera_pivot = get_node(camera_pivot) as Spatial
+export(NodePath) onready var preview_camera = get_node(preview_camera) as Camera
+
+export var rotation_speed: float = 0.2
+
+
+func _physics_process(delta):
+	camera_pivot.rotation_degrees.y += 180*delta*rotation_speed
