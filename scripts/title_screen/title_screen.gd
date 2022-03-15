8f920f2186bd306020b9f8c8a5d87fb4adde149c
lorenzobartolini00 <s1092626@studenti.univpm.it>
2022-03-15 14:01:02 +0100

    Aggiunta preview livello corrente

--- scripts/title_screen/title_screen.gd
+++ scripts/title_screen/title_screen.gd
@@ -4,14 +4,23 @@ class_name TitleScreen
 
 export(NodePath) onready var camera_pivot = get_node(camera_pivot) as Spatial
 export(NodePath) onready var preview_camera = get_node(preview_camera) as Camera
-
+export(NodePath) onready var viewport = get_node(viewport) as Viewport
 
 export var rotation_speed: float = 0.2
 
 
 func _ready():
+	add_level_to_viewport()
+	
 	GameEvents.emit_signal("tab_selected", self)
 	add_to_stack()
+
+
+func add_level_to_viewport() -> void:
+	var level_list_path: Array = Util.list_folder("res://scenes/mockups/")
+	var level = load(level_list_path[SceneManager.current_level_index]).instance()
+	
+	viewport.add_child(level)
 
 
 func _physics_process(delta):
