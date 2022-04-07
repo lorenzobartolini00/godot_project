3ffe2e8db6155a46dd536b87809607457fe53f40
lorenzobartolini00 <s1092626@studenti.univpm.it>
2022-04-07 11:46:58 +0200

    Bug fixes(Update UI) pt.3

--- scripts/UI/button_container_UI.gd
+++ scripts/UI/button_container_UI.gd
@@ -16,7 +16,13 @@ export(bool) onready var active
 
 func _ready():
 	_columns = self.columns
-	_lines = buttons.size() / _columns
+	
+	_lines = 0
+	var button_count: int = buttons.size()
+	
+	while button_count > 0:
+		_lines += 1
+		button_count -= _columns
 	
 	if GameEvents.connect("button_selected", self, "_on_button_selected") != OK:
 		print("failure")
@@ -25,7 +31,7 @@ func _ready():
 	GameEvents.emit_signal("button_selected", buttons[_current_button_index])
 
 
-func _process(delta):
+func _process(_delta):
 	if Input.is_action_just_released("ui_select") and active:
 		GameEvents.emit_signal("button_pressed", buttons[_current_button_index])
 
@@ -37,6 +43,7 @@ func _input(event):
 				var button_selected: bool = false
 				var _new_row: int = _current_button_index/_columns
 				var _new_column: int = _current_button_index%_columns
+				var max_column: int = _columns
 				
 				if Input.is_action_just_pressed("ui_up"):
 					_new_row += (_lines - 1)
@@ -54,8 +61,10 @@ func _input(event):
 					_new_column += 1
 					button_selected = true
 				
+				if _new_row == _lines-1:
+					max_column = buttons.size() - _columns*(_lines-1)
 				
-				_new_column %= _columns
+				_new_column %= max_column
 				
 				if button_selected:
 					
