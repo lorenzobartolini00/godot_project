1bc7034231fe68a727f75415b976234aa55a1e4a
lorenzobartolini00 <s1092626@studenti.univpm.it>
2022-04-18 22:21:35 +0200

    Aggiunte mappe

--- scripts/level1.gd
+++ scripts/level1.gd
@@ -2,28 +2,11 @@ extends Level
 
 class_name Level1
 
-#Obbiettivo del livello: distruggere tutti gli spawner
+#Obbiettivo del livello: trovare uscita
 onready var spawner_count: int = get_tree().get_nodes_in_group("Spawner").size()
 export(Resource) var description = description as Slide
 
 func _ready():
 	spawn_player()
 	
-	if GameEvents.connect("died", self, "_on_died") != OK:
-		print("failure")
-	
-	GameEvents.emit_signal("new_mission", self)
-	
 	global_runtime_data.current_gameplay_state = Enums.GamePlayState.PLAY
-
-
-func _on_died(character: Character):
-	if character is Spawner:
-		spawner_count -= 1
-	
-	call_deferred("check_victory")
-
-
-func check_victory():
-	if spawner_count == 0:
-		win()
