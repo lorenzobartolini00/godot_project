afef0711431b98ed756883578eb6c69df6898834
lorenzobartolini00 <s1092626@studenti.univpm.it>
2022-05-31 22:40:02 +0200

    Added title screen

--- scripts/title_screen/title_screen.gd
+++ scripts/title_screen/title_screen.gd
@@ -2,25 +2,12 @@ extends InteractionTab
 
 class_name TitleScreen
 
-export(NodePath) onready var camera_pivot = get_node(camera_pivot) as Spatial
 export(NodePath) onready var preview_camera = get_node(preview_camera) as Camera
-export(NodePath) onready var viewport = get_node(viewport) as Viewport
 
 export var rotation_speed: float = 0.2
 
 
 func _ready():
-	add_level_to_viewport()
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	
-
-func add_level_to_viewport() -> void:
-	var level_list_path: Array = Util.list_folder("res://scenes/mockups/")
-	var level = load(level_list_path[SceneManager.current_level_index]).instance()
-	
-	viewport.add_child(level)
-
-
-func _physics_process(delta):
-	camera_pivot.rotation_degrees.y += 180*delta*rotation_speed
 	
